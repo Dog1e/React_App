f930d86ad58c6105077d7bf2b7d7b2568c8d0a9d
Michał Szczęsny <michaltablet123@gmail.com>
2023-02-18 13:48:24 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./styles.css";
+import Login from "./components/Login/login";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Reakcja Aplikacja1</h1>
+      <Login />
     </div>
   );
 }
