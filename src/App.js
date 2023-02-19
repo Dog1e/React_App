4f035a4c5c3529406f242202bc62b458e882961b
Michał Szczęsny <73541429+Dog1e@users.noreply.github.com>
2023-02-19 20:14:51 +0100

    Login

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import "./styles.css";
 import Login from "./components/Login/login";
 
-Users = [
-  {
-    username: "Admin",
-    email: "admin@gmail.com",
-    password: "qwerty",
-  },
-];
-
 export default function App() {
   return (
     <div className="App">
--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,11 +1,41 @@
-import { Users } from "../../App";
+var Users = [
+  {
+    username: "Admin",
+    email: "admin@gmail.com",
+    password: "qwerty"
+  }
+];
 
 export default function Login() {
-  function handleLogin() {}
+  const temp = { username: null, password: null };
+  const handleUsername = (event) => {
+    temp.username = event.target.value;
+  };
+
+  const handlePassword = (event) => {
+    temp.password = event.target.value;
+  };
   return (
     <>
       <div>Login Page</div>
-      <div>{Users[1].username}</div>
+      Username
+      <input type="input" onChange={handleUsername} />
+      Password
+      <input type="password" onChange={handlePassword} />
+      <button
+        onClick={() => {
+          for (let i = 0; i < Users.length; i++) {
+            if (
+              Users[i].username === temp.username &&
+              Users[i].password === temp.password
+            ) {
+              console.log(temp);
+            }
+          }
+        }}
+      >
+        Zaloguj
+      </button>
     </>
   );
 }
